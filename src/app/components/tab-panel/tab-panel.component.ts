cacf9b7ccd0ead63d06e588a3a721a595ac4060e
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2025-03-06 16:17:25 -0700

    Set up routes and render some starting components: TabPanel and Home.

---
+++ src/app/components/tab-panel/tab-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-panel',
+  imports: [],
+  standalone: true,
+  templateUrl: './tab-panel.component.html',
+  styleUrl: './tab-panel.component.scss'
+})
+export class TabPanelComponent {
+
+}

f0ead0e742b5cfc215f5a3de38303b48edf7d338
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2025-03-06 16:32:55 -0700

    Add mockup of a TabNav.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabPanelComponent } from "./components/tab-panel/tab-panel.component";
+import { TabNavComponent } from "./components/tab-nav/tab-nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TabPanelComponent],
+  imports: [RouterOutlet, TabPanelComponent, TabNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/tab-nav/tab-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-nav',
+  imports: [],
+  templateUrl: './tab-nav.component.html',
+  styleUrl: './tab-nav.component.scss'
+})
+export class TabNavComponent {
+
+}

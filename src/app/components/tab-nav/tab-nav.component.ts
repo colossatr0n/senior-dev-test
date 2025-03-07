760eb853aa2b1c439c498c5106d58897b5635da4
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2025-03-06 18:41:27 -0700

    Set active state of tab.

--- src/app/components/tab-nav/tab-nav.component.ts
+++ src/app/components/tab-nav/tab-nav.component.ts
@@ -2,19 +2,24 @@ import { Component, inject, OnInit } from '@angular/core';
 import { TabService } from '../../services/tab.service';
 import { Observable, of } from 'rxjs';
 import { Tab } from '../../model/tab.model';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-tab-nav',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NgClass],
   templateUrl: './tab-nav.component.html',
   styleUrl: './tab-nav.component.scss'
 })
 export class TabNavComponent implements OnInit {
     tabService = inject(TabService)
     tabs$: Observable<Tab[]> = this.tabService.getTabs$();
+    activeTab$: Observable<Tab> = this.tabService.getActiveTab()
     
     ngOnInit(): void {
         this.tabs$ = this.tabService.getTabs$()
+    }
+
+    setActive(tab: Tab) {
+        this.tabService.setActive(tab);
     }
 }
--- src/app/services/tab.service.ts
+++ src/app/services/tab.service.ts
@@ -8,14 +8,25 @@ import { Tab } from '../model/tab.model';
 export class TabService {
     tabsSubject = new ReplaySubject<Tab[]>(1) 
     tabs: Tab[] = [
-        new Tab('Home', 'home', 1)
+        new Tab('Home', 'home', 1),
+        new Tab('Home', 'home', 2)
     ]
+    activeTabSubject = new ReplaySubject<Tab>(1)
 
     constructor() {
+        this.setActive(this.tabs[0])
         this.tabsSubject.next(this.tabs)
     }
     
     getTabs$(): Observable<Tab[]> {
         return this.tabsSubject.asObservable().pipe(shareReplay())
+    }
+    
+    getActiveTab(): Observable<Tab> {
+        return this.activeTabSubject.asObservable().pipe(shareReplay())
+    }
+
+    setActive(tab: Tab) {
+        this.activeTabSubject.next(tab)
     }
 }
